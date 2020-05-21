4cd901ac435dd64d9e20594713819e0e80f6295c
James Cowell <james.cowell28@gmail.com>
2020-05-21 16:33:44 +0100

    completed challenge

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by James Cowell on 21/05/2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
